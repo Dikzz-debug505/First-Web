3321124c877bc90d76b1d14dc97e0a7a8a04849c
Dikzz-debug505 <adiilys99@gmail.com>
2026-08-18 18:47:17 +0700

    Add files via upload

--- credentials.js
+++ credentials.js
@@ -1,7 +1,7 @@
 window.MLBB_USERS = [
     { username: "admin", password: "admin123", expiryDate: null },
     { username: "bwi",   password: "bwiteam", expiryDate: "2025-12-31" },
-    { username: "admin2", password: "admin123", expiryDate: null },
+    { username: "adminrido", password: "rido", expiryDate: null },
     // Tambahkan user baru di baris berikutnya, contoh:
     // { username: "userbaru", password: "passwordbaru", expiryDate: "tahun-bulan-tanggal" },
 ];
